02d4cbedc312d92771829e166a17899b7f66627d
Graphite <90666661+GraphiteJP@users.noreply.github.com>
2022-05-04 19:05:55 +0900

    Update Graphite.java

--- Graphite.java
+++ Graphite.java
@@ -1,81 +1,65 @@
+/*
+* やあ！ここはGraphiteのソースコードだよ！(？)
+*/
+
 public class Graphite extends User {
-    public String userName = "Graphite";
-    public String userId = "@GraphiteJP";
-    public int age = 13;
-
-    //　僕だよ！
-    Graphite() {
-        System.out.println(userName + "が降臨した！");
+    public String userName = getUserName();
+    public String getUserName() {
+        return "Graphite";
     }
-
-    public void introduce(int _age) {
-        System.out.println("Hi! My name is " + userName + ", Age is" + _age + "\nNice to meet you!");
-    }
-}
-
-class Subphite extends Graphite {
-    public String subName = "Subphite";
-    public String subId = "@SubphiteJP";
-
-    // サブだよ！
-    Subphite() {
-        System.out.println(subName + "が降臨した...のか？");
-    }
-}
-
-class Thirdphite extends Graphite {
-    public String thirdName = "Thirdphite";
-    public String thirdId = "@ThirdphiteJP";
-
-    // サードだよ！
-    Thirdphite() {
-        System.out.println(thirdName + "が降臨したっぽい");
+    public String userId = getUserId();
+    public String getUserId() {
+        return "@GraphiteJP";
     }
 }
 
 interface Account {
-    // インターフェース
-
     void introduce();
-    void watch(int _length);
     void send(Object _msg);
-    void reply(String _userIdTo, String _userIdFrom, Object _msg);
+    void watch(int _length);
+    void reply(String _toUserName, String _fromUserName, String msg);
     void exit();
 }
 
 abstract class User implements Account {
-    //抽象クラス
-
     public String userName;
     public String userId;
 
-    // メソッド
-    public String getUsername() {
-        return this.userName;
+    @Override
+    public void exit() {
+        System.out.println(this.userName + " exited.");
     }
-    public String getUserId() {
-        return this.userId;
-    }
-
-    // 継承したメソッド
     @Override
     public void introduce() {
-        System.out.println("Hi! My name is " + userName + ".\nNice to meet you!");
+        System.out.printf("Hi! I am %s. Nice to meet you!", this.userName);
     }
     @Override
-    public void watch(int _length) {
-        System.out.println(userName + "is watching Twitter for "+ _length + "minutes...");
+    public void reply(String _toUserName, String _fromUserName, String _msg) {
+        System.out.printf("%s\n|\n%s\n%s", _toUserName, _fromUserName, _msg.toString());
     }
     @Override
     public void send(Object _msg) {
-        System.out.println(userId + " : " + _msg);
+        System.out.printf("%s : %s", this.userName, _msg.toString());
     }
     @Override
-    public void reply(String _userNameTo, String _userNameFrom, Object _msg) {
-        System.out.println(_userNameTo + "\n|\n" + _userNameFrom + "\n" + _msg);
+    public void watch(int _length) {
+        if (_length < 0) {
+            try {
+                throw new NotValidValueException("有効ではない値です");
+            } catch (NotValidValueException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.printf("%s is watching Twitter for %d minutes...", this.userName, _length);
     }
-    @Override
-    public void exit() {
-        System.out.println(userName + "exited.");
+}
+
+// 例外
+
+class NotValidValueException extends Exception {
+    private static final long serialVersionUID = 1l;
+
+    public NotValidValueException(String _msg) {
+        super(_msg);
     }
 }
